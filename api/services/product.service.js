aa03b8c8c028950fd3c40a2196714ef614765e82
Jhon Gomez <jhonjairogomez92@gmail.com>
2025-02-09 18:37:46 -0500

    Add: postgresql

--- api/services/product.service.js
+++ api/services/product.service.js
@@ -1,11 +1,14 @@
 const { faker }  = require('@faker-js/faker');
 const boom = require('@hapi/boom');
 
+const pool = require('../libs/postgres.pool')
 class ProductService {
 
   constructor() {
     this.products = [];
     this.generate();
+    this.pool = pool;
+    this.pool.on('error', (err)=>console.error(err))
   }
 
   async generate() {
@@ -30,12 +33,10 @@ class ProductService {
     return newProduct;
   }
 
-  find() {
-    return new Promise((resolve, reject)=> {
-      setTimeout(()=> {
-        resolve(this.products)
-      },3000)
-    })
+  async find() {
+    const query = 'SELECT * FROM tasks';
+    const rta = await  this.pool.query(query);
+    return rta.rows;
   }
 
   async findOne(id) {
