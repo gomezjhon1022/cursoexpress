6059f258ebdc40cabd6d044aff898ec2bc4dce4f
Jhon Gomez <jhonjairogomez92@gmail.com>
2025-02-22 07:56:20 -0500

    feat: implement one-to-many relationships between Product adn Category models

---
+++ api/db/models/category.model.js
@@ -0,0 +1,48 @@
+const { Model, Sequelize, DataTypes } = require('sequelize');
+
+const CATEGORY_TABLE = 'categories';
+
+const CategorySchema = {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER,
+  },
+  name: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  image: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  createdAt: {
+    allowNull: false,
+    type: DataTypes.DATE,
+    field: 'create_at',
+    defaultValue: Sequelize.fn('NOW'),
+  },
+}
+
+
+class Category extends Model {
+  static associate(models) {
+    this.hasMany(models.Product, {
+      as: 'products',
+      foreignKey: 'categoryid'
+    })
+  }
+
+  static config(sequelize) {
+    return {
+      sequelize,
+      tableName: CATEGORY_TABLE,
+      modelName: 'Category',
+      timestamps: false
+    }
+  }
+}
+
+module.exports = { Category, CategorySchema, CATEGORY_TABLE }
--- api/db/models/index.js
+++ api/db/models/index.js
@@ -1,12 +1,18 @@
-const { User, UserSchema }= require('./user.model');
-const { Customer, CustomerSchema }= require('./customer.model');
+const { User, UserSchema } = require('./user.model');
+const { Customer, CustomerSchema } = require('./customer.model');
+const { Category, CategorySchema } = require('./category.model');
+const { Product, ProductSchema } = require('./product.model');
 
 function setupModels(sequelize) {
   User.init(UserSchema, User.config(sequelize));
   Customer.init(CustomerSchema, Customer.config(sequelize));
+  Customer.init(CategorySchema, Category.config(sequelize));
+  Customer.init(ProductSchema, Product.config(sequelize));
 
   User.associate(sequelize.models);
   Customer.associate(sequelize.models);
+  Category.associate(sequelize.models);
+  Product.associate(sequelize.models);
 }
 
 
